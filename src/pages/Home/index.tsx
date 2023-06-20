be75f3f386fcd6cb8ab51a4ddeaa8e32b7f4986f
Sadraque Cuellar <sadraque.cuellar2001@gmail.com>
2023-06-20 18:51:07 -0400

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import * as S from './styles';
+
+export const Home = () => {
+  return (
+    <S.Container>
+      <Text>Home Screen</Text>
+    </S.Container>
+  )
+}
